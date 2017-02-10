25f47f40ae4818fdbfd1c724fb64e6579487c305
Angie <angiealters@ymail.com>
2017-02-10 17:20:12 -0600

    Added get descendants

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+"use strict";
+
 /*
   Build all of your functions for the application below.
 	Some functions have been stubbed out.
 */
 
 function filterByName(firstName, lastName, people){
-//	return (firstName, lastName, people);
+	var results = people.filter(function(person){
+		return firstName === person.firstName && lastName === person.lastName
+	});
+	return results;	
 }
-//	var nameSearch = data.filter(filterByName);
-
-	// return an array containing the people matching the name
+// return an array containing the people matching the name
+function getDescendants(person, people, descendants=[]){
+	var results = people.filter(function(el){
+		return person.id === el.parents[0] || person.id === el.parents[1]
+	});
+	if (!results){
+		return descendants;
+	}
+	for (var i = 0; i< results.length; i++){
+		descendants.push(results[i]);
+		descendants = getDescendants(results[i], people, descendants)
+	}
+	return descendants;
+}
 
 
 function getFamily(person, people){
@@ -16,7 +32,32 @@ function getFamily(person, people){
 	// return an array containing the family members (objects)
 }
 
-function getDescendants(person, people){
+/*
 
-	// return an array containing the descendants (objects)
+function filterByAge(dob, people){
+	var personAge = people.map(function(getAge){
+		return dob === getAge.dob
+	});
+	return personAge;
 }
+
+function getAge(dateString){
+	var today = new Date();
+	var birthDate = new Date(dateString);
+	var age = today.getFullYear() - birthDate.getFullYear();
+	var month = today.getMonth() - birthDate.getMonth();
+	if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())){
+		age--;
+	}
+	return age;
+}
+
+
+
+function filterByHeight(height, people){
+	var results = people.filter(function(heightInches){
+		return height === heightInches.height
+	});
+	return results;
+}
+*/
--- gui.js
+++ gui.js
@@ -1,24 +1,27 @@
+"use strict";
+
 /*
   Build all of your functions for displaying and gathering information below (GUI).
 */
 
 // app is the function called to start the entire application
 function app(people){
-  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
-  switch(searchType){
-    case 'yes':
+	var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+	switch(searchType){
+		case 'yes':
       // TODO: search by name
-	  searchByName();
-      break;
-    case 'no':
+			var person = searchByName(people);
+			displayPerson(person);
+			mainMenu(person,people);
+			break;
+		case 'no':
       // TODO: search by traits
-	  searchByTrait();
-      break;
-    default:
-	  alert ("Please enter 'yes' or 'no'");
-      app(people); // restart app
-      break;
-  }
+			searchByTrait();
+			break;
+		default:
+			alert("Invalid entry. Please enter 'yes' or 'no'");
+			app(people); // restart app
+	}
 }
 
 // Menu function to call once you find who you are looking for
@@ -31,7 +34,7 @@ function mainMenu(person, people){
     return app(people); // restart
   }
 
-	var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+	var displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
 	switch(displayOption){
 		case "info":
@@ -41,7 +44,10 @@ function mainMenu(person, people){
 			// TODO: get person's family
 			break;
 		case "descendants":
-			// TODO: get person's descendants
+			var descendants = getDescendants(person, people);
+			for(var i = 0; i < descendants.length; i++){
+				alert(descendants[i].firstName +" "+ descendants[i].lastName);
+			}
 			break;
 		case "restart":
 			app(people); // restart
@@ -56,12 +62,19 @@ function mainMenu(person, people){
 function searchByName(people){
   var firstName = promptFor("What is the person's first name?", chars);
   var lastName = promptFor("What is the person's last name?", chars);
-  displayPerson();
+  var person = filterByName(firstName, lastName, people);
+  
+  return person;
   // TODO: find the person using the name they entered
 
 }
+function searchByAge(){
+	
+}
 function searchByTrait(traits){
 	var traitType = promptFor("Search for traits by entering one of the following options: 'age', 'height', 'weight', 'occupation', 'eye color'", selectTrait).toLowerCase();
+
+	
 	switch (traitType){
 		case 'age':
 			promptFor("Please enter person's age", chars);
@@ -95,8 +108,13 @@ function displayPeople(people){
 function displayPerson(person){
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
-  var personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
+  var personInfo = "First Name: " + person[0].firstName + "\n";
+  personInfo += "Last Name: " + person[0].lastName + "\n";
+  personInfo += "Age: " + person[0].dob + "\n";
+  personInfo += "Height: " + person[0].height + "\n";
+  personInfo += "Weight: " + person[0].weight + "\n";
+  personInfo += "Occupation: " + person[0].occupation + "\n";
+  personInfo += "Eye Color: " + person[0].eyeColor + "\n";
   // TODO: finish getting the rest of the information to display
   alert(personInfo);
 }
